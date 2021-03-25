583c9b6098ca4dea9e9d4fd6756105876b27da9c
Matheus Homrich <matidasilva34@gmail.com>
2021-03-25 14:48:32 -0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Matheus Homrich on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
